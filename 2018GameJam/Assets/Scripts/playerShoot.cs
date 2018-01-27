8c728020571595cb572a253657407e9181a4a8c1
Liam Baillie <Leeman1230@gmail.com>
2018-01-26 21:33:44 -0500

    Player can now detect if they're shooting a light or a sound object

---
+++ 2018GameJam/Assets/Scripts/playerShoot.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerShoot : MonoBehaviour {
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////
+    void Start () {
+		
+	}
+	
+	/////////////////////////////////////////////////////////////////////////////////////////////////////
+	void Update () {
+        checkPlayerInput();
+	}
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////
+    //
+    private void checkPlayerInput ()
+    {
+        //left click - for lights
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(transform.position, transform.forward, out hit))
+            {
+                if (hit.collider.tag == "light")
+                {
+                    Debug.Log("Hit Light");
+                }
+            }
+        }
+
+        //right click - for sounds
+        if (Input.GetMouseButtonDown(1))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, transform.forward, out hit))
+            {
+                if (hit.collider.tag == "sound")
+                {
+                    Debug.Log("Hit sound");
+                }
+            }
+        }
+    }
+}
