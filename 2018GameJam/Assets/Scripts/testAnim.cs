6adff9ef231643fe182792614958c9095d63dbaf
Liam Baillie <leeman1230@gmail.com>
2018-01-27 12:51:35 -0500

    Adding sounds and GUI

---
+++ 2018GameJam/Assets/Scripts/testAnim.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testAnim : MonoBehaviour {
+
+    private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+        anim.Play("Door Open");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
