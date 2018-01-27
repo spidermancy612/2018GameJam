45049a7d4ed1bd0b8a6e597e32370d1e9c20759a
NC\akrzywinska1 <redknives13@gmail.com>
2018-01-26 23:51:39 -0500

    game mechanic - launches sound objects gravity changed and jump for player.

---
+++ 2018GameJam/Assets/Scripts/soundLaunchObject.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(universalReciever))]
+
+public class soundLaunchObject : MonoBehaviour {
+
+    universalReciever reciever;
+    bool runOnce;
+    public Vector3 launchAngle;
+    public float launchSpeed;
+    public float objectMass;
+    private Rigidbody rb;
+	
+    
+    ////////////////////////////////////////////////////////////////////////////////////////////
+
+	void Start ()
+    {
+        runOnce = true;
+        reciever = GetComponent<universalReciever>();
+	}
+
+    ////////////////////////////////////////////////////////////////////////////////////////////
+
+    private void Update()
+    {
+        //launches the object in the air if it recieves sound and hasn't been launched before.
+        if (runOnce && reciever.getSound())
+        {
+            runOnce = false;
+            gameObject.AddComponent(typeof(Rigidbody));
+            rb = GetComponent<Rigidbody>();
+            rb.mass = objectMass;
+            rb.AddForce(launchAngle*launchSpeed);
+        }
+    }
+}
