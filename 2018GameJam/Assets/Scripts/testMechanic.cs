b62f4e061715e2b2b686b910fb2d01abf012380a
Liam Baillie <Leeman1230@gmail.com>
2018-01-26 22:51:11 -0500

    Commented temp code

--- 2018GameJam/Assets/Scripts/testMechanic.cs
+++ 2018GameJam/Assets/Scripts/testMechanic.cs
@@ -2,26 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//Make sure there is a universalReciever component on the object
+[RequireComponent(typeof(universalReciever))]
+
 public class testMechanic : MonoBehaviour {
 
-    private universalReciever reciever;
+    private universalReciever reciever;         // Reference for universalReciever component on this object
 
-    public GameObject testObject;
+    public GameObject testObject;               // Object we're effecting 
 
     /////////////////////////////////////////////////////////////////////////////////////////////////////
     void Start () {
+        //Set the component reference for the reciever so we can access it
         reciever = GetComponent<universalReciever>();
 	}
 
     /////////////////////////////////////////////////////////////////////////////////////////////////////
     void Update () {
+        //GetLight() returns the boolean of the light state
+        //If true that means the light is on
 		if (reciever.getLight())
         {
             testObject.SetActive(true);
         }
+        //Otherwise it means the light is off
         else
         {
             testObject.SetActive(false);
+        }
+
+        //Just to show off code
+        if (false)
+        {
+            //This changes the state of the light boolean
+            reciever.updateLight();
         }
 	}
 }
